1c9313e0ff06bc17c7df99c45174875b684f6f93
F2LX <felixfebryan16@gmail.com>
2024-12-03 15:52:14 +0700

    Init: Import initial laravel base & database

---
+++ app/Http/Controllers/BagController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bag;
+use Illuminate\Http\Request;
+
+class BagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Bag $bag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Bag $bag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Bag $bag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bag $bag)
+    {
+        //
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Add Eloquent Relationship
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+
+class Order extends Model
+{
+    // Declares protected variables
+    protected $guarded=['id'];
+
+
+    // Declare Eloquent Relationship
+    public function trashes(): HasMany
+    {
+        return $this->hasMany(Trash::class);
+    }
+
+    public function invoice(): HasOne
+    {
+        return $this->hasOne(Invoice::class);
+    }
+}
