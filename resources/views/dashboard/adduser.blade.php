1dc085a7c5329ef82fb2820abcce1702ea564fce
nirvanaaputri <nirvanaaputri@gmail.com>
2024-12-16 22:11:14 +0700

    add new manage user page

--- resources/views/dashboard/adduser.blade.php
+++ resources/views/dashboard/adduser.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.index')
 
 @section('content')
-    <h2>Hello Anna</h2>
+
+<div class="form-wrapper">
+    <h1 class="form-title">ADD NEW USER</h1>
+    <hr class="line-au">
+    <div class="form">
+        <div class="form-control">
+            <h2 class="input-name">Name</h2>
+            <input type="text" placeholder="Enter your full name">
+            <h2 class="input-name">Email</h2>
+            <input type="text" placeholder="Enter a valid email address">
+        </div>
+        <div class="form-control">
+            <h2 class="input-name">Phone Number</h2>
+            <input type="text" placeholder="Enter your phone number">
+            <h2 class="input-name">Address</h2>
+            <input type="text" placeholder="House number, street name, and postal">
+        </div>
+    </div>
+    <div class="submit-wrapper">
+        <button class="submit-button">Submit</button>
+    </div>
+</div>
+    
 @endsection
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -18,6 +18,12 @@
 
     {{-- Import PickItUp Custom CSS --}}
     <link rel="stylesheet" href="{{ asset('css/adminpanel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adduser.css') }}">
+
+    {{-- Import Font Baloo Bhai 2 & Be Vietnam Pro --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@400..800&family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head> <!--end::Head--> <!--begin::Body-->
 
 <body class="layout-fixed sidebar-expand-lg"> <!--begin::App Wrapper-->
