99949b2a979ddfa4c4731b087eb2b3cc33fc19d4
F2LX <felixfebryan16@gmail.com>
2024-12-16 18:44:57 +0700

    Admin: add dashboard and assets

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+Route::get('/login', function () {
+    return view('loginpage');
+});
+Route::get('/dashboard', function () {
+    return view('layouts.index');
+});
